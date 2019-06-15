e2d23949545d7212bc91485831bb063c1b9e073f
lilyeych <lily.eych@gmail.com>
2019-06-14 23:58:48 -0400

    Updates

---
+++ precache-manifest.da81dac8bd444b301a0f98e1c52220c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531fab512951c014c74387393070f628",
+    "url": "/yujinhan-hy-fullstack-challenge/index.html"
+  },
+  {
+    "revision": "646b07ec03d8c454a7da",
+    "url": "/yujinhan-hy-fullstack-challenge/static/css/main.e045b01b.chunk.css"
+  },
+  {
+    "revision": "57a713cca1249ee4eb8c",
+    "url": "/yujinhan-hy-fullstack-challenge/static/js/2.f8876abc.chunk.js"
+  },
+  {
+    "revision": "646b07ec03d8c454a7da",
+    "url": "/yujinhan-hy-fullstack-challenge/static/js/main.a28742f5.chunk.js"
+  },
+  {
+    "revision": "beca30924ee9e574fb21",
+    "url": "/yujinhan-hy-fullstack-challenge/static/js/runtime~main.d6dc1636.js"
+  },
+  {
+    "revision": "22af8ff1419ab3b085608e48ae299d1e",
+    "url": "/yujinhan-hy-fullstack-challenge/static/media/the-movie-db-logo.22af8ff1.svg"
+  }
+]);
