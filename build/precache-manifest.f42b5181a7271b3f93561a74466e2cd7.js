10c8d15f35f96dfc74f59b40f300ffa2ea64b11b
Lily Han <lily.han@fcbsixconsulting.com>
2019-06-18 12:13:23 -0400

    prodution build

---
+++ build/precache-manifest.f42b5181a7271b3f93561a74466e2cd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a245ebc18c1fca9075d2a312c7ec8f",
+    "url": "/yujinhan-hy-fullstack-challenge/index.html"
+  },
+  {
+    "revision": "ac819b1285e66616649b",
+    "url": "/yujinhan-hy-fullstack-challenge/static/css/main.7e3188c9.chunk.css"
+  },
+  {
+    "revision": "57a713cca1249ee4eb8c",
+    "url": "/yujinhan-hy-fullstack-challenge/static/js/2.f8876abc.chunk.js"
+  },
+  {
+    "revision": "ac819b1285e66616649b",
+    "url": "/yujinhan-hy-fullstack-challenge/static/js/main.7852d696.chunk.js"
+  },
+  {
+    "revision": "beca30924ee9e574fb21",
+    "url": "/yujinhan-hy-fullstack-challenge/static/js/runtime~main.d6dc1636.js"
+  },
+  {
+    "revision": "22af8ff1419ab3b085608e48ae299d1e",
+    "url": "/yujinhan-hy-fullstack-challenge/static/media/the-movie-db-logo.22af8ff1.svg"
+  }
+]);
